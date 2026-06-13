f6bfe4f69e45f0745133ee120a8abbeedd3ab918
ajaykaushikmacmini-create <ajaykaushikmacmini@gmail.com>
2026-06-13 10:18:36 +0530

    Update GEMINI_API_KEY in config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 // par production mein API key ko backend/proxy se use karna best hota hai.
 // ====================================================================
 window.HINDIDIWAS_CONFIG = {
-  GEMINI_API_KEY: "AQ.Ab8RN6IsBHjFTjjm8Px-lHHV4x4TEPKI7EtEOStwqnZCdl6ZnA",
+  GEMINI_API_KEY: "AQ.Ab8RN6Ln_g8gZ1F8beDyIKCODhpJ4PNzXgmZ0-3GRHZEcZJo0g",
   GEMINI_MODEL: "gemini-2.5-flash"
 };
